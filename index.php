7873a1cfd1fae1b860c4641df1e5837b47de022d
elarcrex <nghaka.chawngthu@gmail.com>
2016-07-13 09:39:37 -0700

    Added more useful comments

--- index.php
+++ index.php
@@ -6,9 +6,19 @@ require 'src/Bar.php';
 require 'src/Baz.php';
 require 'helpers/functions.php';
 
+/*
+Bind hmasak emaw
+
 bind('Foo', function(){
 	return new Foo(new Bar, new Baz);
 });
 
 var_dump(app('Foo'));
+*/
 
+/*
+Bind lem lovin
+var_dump(app('Foo'));
+*/
+
+$foo = app('Foo');
